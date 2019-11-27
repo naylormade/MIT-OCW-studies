266183bc5a062e2cee4cf5f661a5ec45b26ad328
Daniel Naylor <naylordaniel@yahoo.com>
2019-11-27 11:08:05 -0800

    renamed ps1 to ps1a, finished ps1b

---
+++ 6-001 - Python/ps1a.py
@@ -0,0 +1,19 @@
+total_months = 0
+
+total_cost = float(input("Cost of dream home"))
+annual_salary = float(input("Starting annual salary?"))
+portion_saved = float(input("Portion to save? (decimal)"))
+current_savings = 0
+portion_down_payment = 0.25
+
+#annual_savings = annual_salary * portion_saved
+#annual_returns = current_savings * 0.04/12.0
+
+down_payment = total_cost * portion_down_payment
+
+while current_savings < down_payment:
+    annual_returns = current_savings * 0.04/12.0
+    current_savings += annual_returns + ((annual_salary / 12) * portion_saved)
+    total_months += 1
+
+print(f'Number of months: {total_months}')
--- 6-001 - Python/ps1b.py
+++ 6-001 - Python/ps1b.py
@@ -1,8 +1,9 @@
 total_months = 0
 
-total_cost = 1000000.0  #float(input("Cost of dream home"))
+total_cost = 500000.0  #float(input("Cost of dream home"))
 annual_salary = 120000.0  #float(input("Starting annual salary?"))
-portion_saved = .1  #float(input("Portion to save? (decimal)"))
+portion_saved = .05  #float(input("Portion to save? (decimal)"))
+semi_annual_raise = .03  #float(input("What is your semi-annual raise percentage? (decimal)"))
 current_savings = 0
 portion_down_payment = 0.25
 
@@ -15,5 +16,7 @@ while current_savings < down_payment:
     annual_returns = current_savings * 0.04/12.0
     current_savings += annual_returns + ((annual_salary / 12) * portion_saved)
     total_months += 1
+    if total_months % 6 == 0:
+        annual_salary *= (1 + semi_annual_raise)
 
 print(f'Number of months: {total_months}')
